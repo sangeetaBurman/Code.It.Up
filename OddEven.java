216360379b9729a078ed2101d9c907b3d7b80e7f
Sangeeta Burman <sangeetaburman2010@gmail.com>
2019-08-14 23:27:23 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -6,11 +6,11 @@ public static void main(String args[])
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the number");
 int a=sc.nextInt();
-if((a%2)==0)
+if((a%2)==0 && a>0)
 {
 System.out.println("Even");
 }
-else if((a%2)!=0)
+else if((a%2)!=0 && a>0)
 {
 System.out.println("Odd");
 }
