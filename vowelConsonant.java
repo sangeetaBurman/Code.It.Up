c164ae21c65173403bf7453319c3e467eba3be82
Sangeeta Burman <sangeetaburman2010@gmail.com>
2019-08-15 17:00:15 +0530

    Create vowelConsonant.java

---
+++ vowelConsonant.java
@@ -0,0 +1,24 @@
+import java.util.*
+class vowelConsonant
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner;
+Sysytem.out.println("Enter the alphabet");
+String s=sc.next();
+char a[] = {a,e,i,o,u}
+if(s==a[])
+{
+System.out.println("Vowel");
+}
+else if(s!=a[])
+{
+System.out.println("Consonant");
+}
+else
+{
+System.out.println("Invalid");
+}
+}
+
+}
