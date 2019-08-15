b3b5f11c001bb03d4098c851ef09d061fbf0d70e
Sangeeta Burman <sangeetaburman2010@gmail.com>
2019-08-15 17:05:53 +0530

    Update vowelConsonant.java

--- vowelConsonant.java
+++ vowelConsonant.java
@@ -1,24 +1,24 @@
-import java.util.*
+import java.util.*;
 class vowelConsonant
 {
 public static void main(String args[])
 {
-Scanner sc=new Scanner;
-Sysytem.out.println("Enter the alphabet");
-String s=sc.next();
-char a[] = {a,e,i,o,u}
-if(s==a[])
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter an alphabet");
+char ch=sc.next().charAt(0);
+if(ch=='a'||ch=='A'|| ch=='e'|| ch=='E'|| ch=='i'|| ch=='I'|| 
+ch=='o'|| ch=='O'|| ch=='u'|| ch=='U')
 {
 System.out.println("Vowel");
 }
-else if(s!=a[])
+else if(ch>='a' && ch<='z')
 {
+
 System.out.println("Consonant");
 }
-else
+else 
 {
 System.out.println("Invalid");
 }
 }
-
-}
+} 
